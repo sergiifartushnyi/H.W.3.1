ba228a2808aa18f1421924ced83aebc3640f3c24
sergiifartushnyi <sergiyfartyshnyy@gmail.com>
2024-07-28 00:07:47 +0300

    home work 3.1

---
+++ 3_1.py
@@ -0,0 +1,25 @@
+import math1
+
+print("Вітаємо у калькуляторі")
+
+n1 = int(input("Введіть перше число:"))
+
+operation = input("Виберіть дію (+, -, *, /): ")
+
+n2 = int(input("Введіть друге число:"))
+
+def calculate(n1,operation,n2):
+    if operation == "+":
+        return n1 + n2
+    elif operation == "-":
+        return n1 - n2
+    elif operation == "*":
+        return n1 * n2
+    elif operation =="/":
+        if n2 == 0:
+            return "Увага! Дільник не може дорівнювати 0!"
+        return n1 / n2
+
+result = calculate(n1,operation, n2)
+
+print(f"Відповідь: {result}")
